66685f9a9a630133f1b31e0bbda6bde4ba33d6a6
Yasuhiko UEHARA <y-uehara@bloodymary.net>
2020-05-20 23:13:24 +0900

    Raspberry PI向けにビルドした場合の解像度設定を追加

--- SizeSettings.cpp
+++ SizeSettings.cpp
@@ -2,9 +2,17 @@
 
 SizeSettings::SizeSettings(QObject *parent) : QObject(parent)
 {
+#ifdef RASPBERRYPI
+    m_window_width = 640;
+    m_window_height = 480;
+
+    m_canvas_width = 480;
+    m_canvas_height = 480;
+#else
     m_window_width = 1280;
     m_window_height = 960;
 
     m_canvas_width = 960;
     m_canvas_height = 960;
+#endif
 }
